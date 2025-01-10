ed384d716fbdab0a6183868b6f6aa74ec069af31
cooolgamer <64099608+cooolgamer@users.noreply.github.com>
2025-01-10 16:00:16 +0100

    Fix spaces in the path not working

--- Custom Keyboard Patcher.bat
+++ Custom Keyboard Patcher.bat
@@ -49,10 +49,11 @@ FOR %%S in (SystemApp.0000.*) do (
 	set cxi0=%%~nxS
 )
 3dstool.exe -xvtf cxi %cxi0% --header ncchheader.bin --exh exheader.bin --exefs exefs.bin --romfs romfs.bin --plain plainrgn.bin
-copy /y %DESTDIR%romfs-patched.bin romfs.bin
+pause
+copy /y "%DESTDIR%romfs-patched.bin" romfs.bin
 3dstool.exe -cvtf cxi %cxi0% --header ncchheader.bin --exh exheader.bin --exefs exefs.bin --romfs romfs.bin --plain plainrgn.bin
 makerom.exe -f cia -content %cxi0%:0 -o "%DESTDIR%%OUTNAME%.cia" -major 4 -minor 0
-
+pause
 echo Cleaning up...
 del /q SystemApp.0000.*
 del /q *.bin
